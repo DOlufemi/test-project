02baf99d24a0eaf5ac619cd0f56e498ddfb7d6c1
Iavael <iavael@users.noreply.github.com>
2019-08-18 20:36:30 +0300

    Parametrize batch settings

--- main.go
+++ main.go
@@ -11,21 +11,25 @@ import (
 )
 
 var (
-	debug bool
-	spath string
-	addr  = "[::1]:8080"
+	debug    bool
+	spath    string
+	maxbatch int
+	maxtime  int
+	addr     = "[::1]:8080"
 )
 
 func main() {
 	flag.BoolVar(&debug, "d", false, "Enable debug logging")
 	flag.StringVar(&spath, "s", "storage.txt", "Path to storage file")
+	flag.IntVar(&maxbatch, "b", 1000, "Max size of batch written to storage")
+	flag.IntVar(&maxtime, "t", 5, "Max time before data is flushed from buffer")
 	flag.Parse()
 
 	if flag.NArg() > 0 {
 		addr = flag.Arg(0)
 	}
 
-	storage, err := NewStorage(spath, 1000, 5*time.Second)
+	storage, err := NewStorage(spath, maxbatch, time.Duration(maxtime)*time.Second)
 	if err != nil {
 		log.Fatalf("Failed to open storage: %s\n", err)
 	}
